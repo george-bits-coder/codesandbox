dbcedb43e0241ae2cdcc45197f787f3e24a46bee
george-bits-coder <91771684+george-bits-coder@users.noreply.github.com>
2021-12-30 23:13:12 +0530

    counter app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import { useState } from "react";
 import "./styles.css";
 
-export default function App() {
+
+
+ function App() {
+
+  const [count,setCount]=useState(0);
+
+  const addOne=(value)=>{
+
+    setCount(count+value);
+  }
+
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Counter {count}</h1>
+     
+     
+  <button onClick={()=>addOne(1)}>Increase</button>
+  {"     "}
+  <button onClick={()=>addOne(-1)}>Decrease</button>
+
+{"     "}
+
+  <button onClick={()=>addOne(count)}>Double</button>  
     </div>
   );
 }
+
+
+
+export default App;
